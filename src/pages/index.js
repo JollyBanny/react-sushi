6b9d0d2a6e81e188819c3b22340c11ac4342a0dd
JollyBanny <rootki7@protonmail.com>
2021-12-16 21:53:57 +1000

    add pages and connect with routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,4 @@
 export { default as Home } from "./Home";
+export { default as About } from "./About";
+export { default as Delivery } from "./Delivery";
+export { default as Catalog } from "./Catalog";
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Home } from "../pages";
+
+import { useDispatch } from "react-redux";
+import { setCategory } from "../redux/actions/category";
+
+import { Home, About, Delivery, Catalog } from "../pages";
+import { Cart, MenuBar } from "../components";
 
 function Pages() {
+  const dispatch = useDispatch();
+  const onSelectCategory = React.useCallback((category) => {
+    dispatch(setCategory(category));
+  }, [dispatch]);
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <div className="main">
+      <div className="menu">
+        <div className="container">
+          <MenuBar onClickCategory={onSelectCategory} />
+          <Cart />
+        </div>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home onClickCategory={onSelectCategory} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/delivery" element={<Delivery />} />
+        <Route path="/catalog" element={<Catalog />}>
+          <Route path="/catalog/:id" element={<Catalog />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
